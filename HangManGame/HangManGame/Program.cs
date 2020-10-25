610207f0330cae39711333ced3fbdfc4961edd55
anwatson <anwatson1982@gmail.com>
2020-10-25 23:37:09 +0000

    Created HangMan project, at the moment got to the stage where I have a list of words and have a function to select a random number tht will used to select a random word from the list.

---
+++ HangManGame/HangManGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///create list of words to use for hangman game
+            List<string> HangManWords = new List<string>();
+            HangManWords.Add("computer");
+            HangManWords.Add("laptop");
+            HangManWords.Add("phone");
+            HangManWords.Add("football");
+            HangManWords.Add("rugby");
+            HangManWords.Add("apple");
+            HangManWords.Add("house");
+            ///Create Random number to pick from the list of words 
+            var randNum = new Random();
+            int pickWord = randNum.Next(0, HangManWords.Count);
+            Console.WriteLine(HangManWords[pickWord]);
+        }
+    }
+}
