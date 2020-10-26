cba63fc31537b841310d088794ce63513e94ba90
anwatson <anwatson1982@gmail.com>
2020-10-26 02:07:16 +0000

    Created function to check if a letter inputted by the user is part of the Randomly selected word the user has to guess

--- HangManGame/HangManGame/Program.cs
+++ HangManGame/HangManGame/Program.cs
@@ -7,6 +7,7 @@ namespace HangMan
     {
         static void Main(string[] args)
         {
+            string inputLetter = "";
             ///create list of words to use for hangman game
             List<string> HangManWords = new List<string>();
             HangManWords.Add("computer");
@@ -20,6 +21,30 @@ namespace HangMan
             var randNum = new Random();
             int pickWord = randNum.Next(0, HangManWords.Count);
             Console.WriteLine(HangManWords[pickWord]);
+            string newWord = (HangManWords[pickWord]);
+            Console.WriteLine($"Select a letter");
+            inputLetter = Console.ReadLine();
+            bool checkLetter = CheckWord(newWord, inputLetter);
+            if (checkLetter == true)
+            {
+                Console.WriteLine($"You got a letter");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect letter try again");
+            }
+
+            static bool CheckWord(string n, string i)
+            {
+                if (n.Contains(i))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 }
