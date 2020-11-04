e4a67370264175a037405f30a3616593c955a6f7
anwatson1982 <60107609+anwatson1982@users.noreply.github.com>
2020-11-04 20:03:49 +0000

    Update Program.cs

--- HangManGame/HangManGame/Program.cs
+++ HangManGame/HangManGame/Program.cs
@@ -1,60 +1,119 @@
-﻿using System;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
-namespace HangMan
+namespace HangMan2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string inputLetter = "";
-            int lives;
-            ///create list of words to use for hangman game
             List<string> HangManWords = new List<string>();
-            HangManWords.Add("computer");
-            HangManWords.Add("laptop");
-            HangManWords.Add("phone");
-            HangManWords.Add("football");
-            HangManWords.Add("rugby");
+            HangManWords.Add("Computer");
+            HangManWords.Add("television");
             HangManWords.Add("apple");
-            HangManWords.Add("house");
-            ///Create Random number to pick from the list of words 
-            var randNum = new Random();
-            int pickWord = randNum.Next(0, HangManWords.Count);
-            string newWord = (HangManWords[pickWord]);
-            Console.WriteLine(newWord);
-        /// for loop that works out amount of lives users has had, if they guess an incorrect 
-        /// letter they will lose a life and get asked to choose another letter 
-            for (lives = 5; lives > 0; )
+            HangManWords.Add("jumper");
+            HangManWords.Add("flask");
+            HangManWords.Add("laptop");
+            HangManWords.Add("brother");
+            HangManWords.Add("sister");
+            HangManWords.Add("flowers");
+            HangManWords.Add("pictures");
+            HangManWords.Add("curtains ");
+
+            int lives;
+            string PlayGame = "";
+            var RandNum = new Random();
+            int SelectWord = RandNum.Next(0, HangManWords.Count);
+            string SecretWord = (HangManWords[SelectWord]);
+            char[] SecretWordArray = SecretWord.ToCharArray();
+            WelcomDisplay();
+            PlayGame = Console.ReadLine();
+            if (PlayGame == "x")
             {
-                int wordLength = newWord.Length;
-                Console.WriteLine(wordLength);
-                Console.WriteLine($"Select a letter");
-                inputLetter = Console.ReadLine();
-                bool checkLetter = CheckWord(newWord, inputLetter);
-                if (checkLetter == true)
-                {
-                   
-                    Console.WriteLine($"You got a letter");
-                }
-                else
-                {
-                    Console.WriteLine($"Incorrect letter try again");
-                    lives = lives - 1;
-                }
+                EndGameDisplay();
+                return;
             }
-            ///Function to work out if the users letter inoutted is part of the word 
-            static bool CheckWord(string n, string i)
+            else
             {
-                if (n.Contains(i))
+                Console.Clear();
+                DisplaySecretWord(SecretWordArray);
+                Console.WriteLine();
+                
+                for (lives = 10; lives > 0;)
+                    
                 {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                    RequestLetterDisplay();
+                    string InputLetter = Console.ReadLine();
+                    List<string> LetterList = new List<string>();
+                    LetterList.Add(InputLetter);
+                    bool WordCheck = CheckWord(SecretWord, InputLetter);
+                    int NewLifeCount = LivesCounter(lives, WordCheck);
+                    DisplayLives(NewLifeCount);
+                    DisplayResult(WordCheck);
+                } 
+
             }
+        }
+        static void WelcomDisplay()
+        {
+            Console.WriteLine($"Press any key to enter game press [x] to quit");
+            Console.WriteLine($"Enter a letter you think is in the secret word");
+            Console.WriteLine($"If you guess a correct letter the word will show itself");
+            Console.WriteLine($"If your guess is incorrect you lose one of your 10 lives");
+            Console.WriteLine($"Try to guess the word before you lose all of your live");
+        }
+        static void EndGameDisplay()
+        {
+            Console.WriteLine($"Game over goodbye :)");
+        }
+        static void RequestLetterDisplay()
+        {
+            Console.WriteLine($"Please enter a Letter you think is in the secret word");
+        }
+        static void DisplaySecretWord(char[] WordArray)
+        {
+            foreach (char LetterArray in WordArray)
+            {
+                Console.Write($" {LetterArray} ");
+            }
+        }
+        static bool CheckWord(string Word, string Letter)
+        {
+            if (Word.Contains(Letter))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void DisplayResult(bool WordChecked)
+        {
+            if (WordChecked == true)
+            {
+                Console.WriteLine($"You got a letter");
+            }
+            else
+            {
+                Console.WriteLine($"The letter you picked is not in the sectret word please try again");
+            }
+        }
+        static int LivesCounter (int trys, bool WordChecked)
+        {
+            if (WordChecked == false)
+            {
+                trys = trys - 1;
+                return trys;
+            }
+            else
+                return trys;
+                        
+        }
+        static void DisplayLives (int trys)
+        {
+            Console.WriteLine($"You have {trys} left");
         }
     }
 }
